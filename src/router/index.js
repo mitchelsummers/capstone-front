70718649a85473c9334f9d3379971b8bd76d8a52
Mitchel Summers <mitchel.a.summers@gmail.com>
2021-12-14 11:30:06 -0700

    configures router for views

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,21 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
+import CardsEdit from "../views/CardsEdit.vue";
+import CardsIndex from "../views/CardsIndex.vue";
+import CardsNew from "../views/CardsNew.vue";
+import CardsShow from "../views/CardsShow.vue";
+import CoursesEdit from "../views/CoursesEdit.vue";
+import CoursesIndex from "../views/CoursesIndex.vue";
+import CoursesNew from "../views/CoursesNew.vue";
+import CoursesShow from "../views/CoursesShow.vue";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Signup from "../views/Signup.vue";
+import UserHome from "../views/UserHome.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: "/",
-    name: "Home",
-    component: Home,
-  },
   {
     path: "/about",
     name: "About",
@@ -18,6 +24,18 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
+  { path: "/CardsEdit", name: "CardsEdit", component: CardsEdit },
+  { path: "/CardsIndex", name: "CardsIndex", component: CardsIndex },
+  { path: "/CardsNew", name: "CardsNew", component: CardsNew },
+  { path: "/CardsShow", name: "CardsShow", component: CardsShow },
+  { path: "/CoursesEdit", name: "CoursesEdit", component: CoursesEdit },
+  { path: "/CoursesIndex", name: "CoursesIndex", component: CoursesIndex },
+  { path: "/CoursesNew", name: "CoursesNew", component: CoursesNew },
+  { path: "/CoursesShow", name: "CoursesShow", component: CoursesShow },
+  { path: "/", name: "Home", component: Home },
+  { path: "/Login", name: "Login", component: Login },
+  { path: "/Signup", name: "Signup", component: Signup },
+  { path: "/UserHome", name: "UserHome", component: UserHome },
 ];
 
 const router = new VueRouter({
